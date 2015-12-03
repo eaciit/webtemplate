b92e2bd59c5da241dbfd75e8d8279689fe687869
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2015-12-03 13:51:02 +0700

    web template using knot v.1 and dbox json in folder config

--- eaciittemplate.go
+++ eaciittemplate.go
@@ -1,8 +1,12 @@
 package eaciittemplate
 
 import (
+	"fmt"
+	"github.com/eaciit/dbox"
+	_ "github.com/eaciit/dbox/dbc/json"
 	// "github.com/eaciit/knot"
-	"github.com/eaciit/knot/appcontainer"
+	// "github.com/eaciit/knot/appcontainer"
+	"github.com/eaciit/knot/knot.v1"
 	// "github.com/eaciit/toolkit"
 	"os"
 	// "time"
@@ -16,13 +20,98 @@ var (
 )
 
 func init() {
-	app := appcontainer.NewApp("Webtemplate")
+	app := knot.NewApp("Webtemplate")
 	app.ViewsPath = appViewsPath
 	app.Register(&TemplateController{})
 	app.Static("static", "/Users/arfianbagus/Documents/go/src/github.com/eaciit/webtemplate/")
 	app.LayoutTemplate = "index.html"
-	appcontainer.RegisterApp(app)
+	knot.RegisterApp(app)
+
+	// ks := new(knot.Server)
+	// ks.Address = "localhost:7878"
+	// DefaultOutputType = OutputHtml
+	// ks.Register(new(TemplateController), "")
+	// ks.RouteStatic("static", "/Users/arfianbagus/Documents/go/src/github.com/eaciit/webtemplate/")
+	// ks.Listen()
 }
 
 type TemplateController struct {
+}
+
+func prepareConnection(pathJson string) (dbox.IConnection, error) {
+	ci := &dbox.ConnectionInfo{pathJson, "", "", "", nil}
+
+	c, e := dbox.NewConnection("json", ci)
+	if e != nil {
+		fmt.Printf("Yo to connect %s \n", e.Error())
+		return nil, e
+	}
+
+	e = c.Connect()
+	if e != nil {
+		fmt.Printf("Yoi to connect %s \n", e.Error())
+		return nil, e
+	}
+
+	return c, nil
+}
+
+func (w *TemplateController) GetMenuTop(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+	c, e := prepareConnection("/Users/arfianbagus/Documents/go/src/github.com/eaciit/webtemplate/config/topmenu.json")
+	if e != nil {
+		fmt.Printf("Unable to connect %s \n", e.Error())
+	}
+	defer c.Close()
+
+	csr, e := c.NewQuery().Select("title").Cursor(nil)
+
+	if e != nil {
+		fmt.Printf("Cursor pre error: %s \n", e.Error())
+	}
+	if csr == nil {
+		fmt.Printf("Cursor not initialized")
+	}
+
+	defer csr.Close()
+
+	//rets := []toolkit.M{}
+
+	ds, e := csr.Fetch(nil, 0, false)
+	if e != nil {
+		fmt.Printf("Unable to fetch all: %s \n", e.Error())
+	} else {
+		fmt.Printf("Fetch all OK. Result: %d \n", len(ds.Data))
+	}
+	return ds.Data
+}
+
+func (w *TemplateController) GetMenuLeft(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+	c, e := prepareConnection("/Users/arfianbagus/Documents/go/src/github.com/eaciit/webtemplate/config/leftmenu.json")
+	if e != nil {
+		fmt.Printf("Unable to connect %s \n", e.Error())
+	}
+	defer c.Close()
+
+	csr, e := c.NewQuery().Select("titlegroup").Cursor(nil)
+
+	if e != nil {
+		fmt.Printf("Cursor pre error: %s \n", e.Error())
+	}
+	if csr == nil {
+		fmt.Printf("Cursor not initialized")
+	}
+
+	defer csr.Close()
+
+	//rets := []toolkit.M{}
+
+	ds, e := csr.Fetch(nil, 0, false)
+	if e != nil {
+		fmt.Printf("Unable to fetch all: %s \n", e.Error())
+	} else {
+		fmt.Printf("Fetch all OK. Result: %d \n", len(ds.Data))
+	}
+	return ds.Data
 }
