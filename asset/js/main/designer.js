97cdc7cfd76ab553ab5ea311c062745f7c667db4
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2015-12-30 11:39:22 +0700

    hideshow panel

--- asset/js/main/designer.js
+++ asset/js/main/designer.js
@@ -19,6 +19,24 @@ viewModel.designer.addPanel = function (title, widthRatio, height) {
 viewModel.designer.config = function (o) {
 
 };
+viewModel.designer.hideShow = function(e){
+	var x_panel = $(e).closest('div.panel-primary'), button = x_panel.find('i.hideshow'),content = x_panel.find('div.panel-body');
+    content.slideToggle(200);
+	button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+	if(button.hasClass('fa-chevron-up')){
+        $(x_panel).animate({height:parseInt($(x_panel).attr('heightContent'))},200, function() {
+            $(".grid-container").trigger("ss-rearrange");
+        });
+    } else {
+        $(x_panel).animate({height:40},200, function() {
+            $(".grid-container").trigger("ss-rearrange");
+        });
+    }
+	x_panel.toggle
+    setTimeout(function () {
+        x_panel.resize();
+    }, 50);
+}
 
 $(function () {
 	viewModel.designer.prepare();
